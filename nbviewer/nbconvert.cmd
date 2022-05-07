590891280d3c12359aede7493a8473ef48b70838
playGitboy <72111157+playGitboy@users.noreply.github.com>
2022-05-07 11:29:41 +0800

    Update and rename nbviewer.cmd to nbconvert.cmd

--- nbviewer/nbconvert.cmd
+++ nbviewer/nbconvert.cmd
@@ -1,3 +1,3 @@
 rem Please add jupyter to the system environment variable.
 rem 请提前将jupyter所在目录加入环境变量
-jupyter nbconvert --to html --embed-images %1 --output %2
+jupyter nbconvert --to html --embed-images %1 --output %
